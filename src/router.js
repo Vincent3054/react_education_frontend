b6983677071da486a02ef73942cbaa4ff9530a1b
unknown <ok96305@gmail.com>
2020-04-13 17:01:58 +0800

    new loging

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import Index from "./routes/Index";
+import Loging from"./routes/Loging";
 
 export default (
   <HashRouter>
     <Switch>
       <Route path="/" exact component={Index} />
+      <Route path="/Loging" exact component={Loging} />
     </Switch>
   </HashRouter>
 );
---
+++ src/routes/Loging.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Layout from '../layouts/Layout';
+import './Index.css';
+
+export default class Loging extends Component {
+    render() {
+        return (
+            <Layout>
+                <div>123</div>
+            </Layout>
+        );
+    }
+}
