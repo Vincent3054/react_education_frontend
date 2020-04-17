63c70b1bca0ea9970f48cf548d32fb1235e30a73
yuchun <yuchun@youteki-MacBook-Air.local>
2020-04-17 20:13:12 +0800

    update 4/17

---
+++ src/routes/Studentdata.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+import Layout from '../layouts/Layout';
+import './Studentdata.css';
+
+export default class Studentdata extends Component {
+    render() {
+
+        return (
+            <Layout>
+                <div className="Studentdata-body">
+                    <div>
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </Layout>
+        );
+    }
+}
