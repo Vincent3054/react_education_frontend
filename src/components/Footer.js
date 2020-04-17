285cf88518e5bedca3db9ecdf489b47d72329a04
yuchun <yuchun@youteki-MacBook-Air.local>
2020-04-17 14:10:53 +0800

    漢堡條做好了耶～4/17

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,23 +5,7 @@ export default class Footer extends Component {
   render() {
     return (
       <div className='footer'>
-      <div>
-      <hr />
-      <ul>
-      <li className='footer-list'><a href="https://www.instagram.com" target="_blank"><img src="https://img.walei.tw/images/index/icon_ig.png" width="10px"/></a>instagram</li>
-      <li className='footer-list'><a href="https://line.me/R/ti/p/M0NmOIYJLP" target="_blank"><img src="https://img.walei.tw/images/index/icon_line.png" width="10"/></a>line</li>
-      <li className='footer-list'><a href="https://www.youtube.com" target="_blank"><img src="https://img.walei.tw/images/index/icon_youtube.png" width="10"/></a>youtube</li>
-      <li className='footer-list'><a href="https://www.facebook.com" target="_blank"><img src="https://img.walei.tw/images/index/icon_fb.png" width="10"/></a>facebook</li>
-      <li className='footer-font'>關於新聞中心版權</li>
-      <li className='footer-font'>與我們聯絡創作者廣告</li>
-      <li className='footer-font'>開發人員</li>
-      <li className='footer-font'>條款隱私權政策與安全性</li>
-      <li className='footer-font'>測試新功能</li>
       
-      </ul>
-      
-
-      </div>
       </div>
     );
   }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,22 +1,61 @@
 import React, { Component } from 'react';
 import './Menu.css';
-import user from './images/user.png';
-import edit from './images/edit.png';
-import home from './images/home.png';
-import set from './images/settings.png';
+import user from '../Assets/user (1).png';
+import home from '../Assets/home.png';
+import edit from '../Assets/edit.png';
+import set from '../Assets/settings.png';
+import menu from '../Assets/open-menu (1).png';
 
-export default class Header extends Component {
+
+export default class Menu extends Component {
+
+  state = {
+    ac: false
+  }
+  handelActive = () => {
+    const { ac } = this.state;
+    if (ac == false) {
+      this.setState({ ac: true });
+    } else {
+      this.setState({ ac: false });
+    }
+  }
   render() {
+    const { ac } = this.state;
     return (
-      <div className='Sidebar'>
-        <div class='Block-one'>
-          <ul className='Menu-ul'>
-            <li class='Menu-list'><img src={home} width='20px' className='Menu-img'/>首頁</li>
-              <li class='Menu-list'><img src={user} width='20px' className='Menu-img'/>基本資料</li>
-                <li class='Menu-list'><img src={edit} width='20px' className='Menu-img'/>編輯資料</li>
-                <li class='Menu-list'><img src={set} width='20px' className='Menu-img'/>設定</li>
-               </ul>
-         </div>
+      <div>
+
+        <button onClick={this.handelActive} className='Menu-buger' ><img src={menu} width='20px' className='Menu-buger-img' /></button>
+
+        <div className={ac ? `Sidebar active` : `Sidebar`}>
+          <div className='Block-one'>
+            <ul className='Menu-ul'>
+              <li className='Menu-list'><img src={home} width='20px' className='Menu-img' />首頁</li>
+              <li className='Menu-list'><img src={user} width='20px' className='Menu-img' />基本資料</li>
+              <li className='Menu-list'><img src={edit} width='20px' className='Menu-img' />編輯資料</li>
+              <li className='Menu-list'><img src={set} width='20px' className='Menu-img' />設定</li>
+            </ul>
+          </div>
+          <div className='footer'>
+
+            <hr />
+            <ul>
+              <li className='footer-list'><a href="https://www.instagram.com" target="_blank"><img src="https://img.walei.tw/images/index/icon_ig.png" className="footer-img" width="10px" /></a>instagram</li>
+              <li className='footer-list'><a href="https://line.me/R/ti/p/M0NmOIYJLP" target="_blank"><img src="https://img.walei.tw/images/index/icon_line.png" className="footer-img" width="10" /></a>line</li>
+              <li className='footer-list'><a href="https://www.youtube.com" target="_blank"><img src="https://img.walei.tw/images/index/icon_youtube.png" className="footer-img" width="10" /></a>youtube</li>
+              <li className='footer-list'><a href="https://www.facebook.com" target="_blank"><img src="https://img.walei.tw/images/index/icon_fb.png" className="footer-img" width="10" /></a>facebook</li>
+              <li className='footer-font'>關於新聞中心版權</li>
+              <li className='footer-font'>與我們聯絡創作者廣告</li>
+              <li className='footer-font'>開發人員</li>
+              <li className='footer-font'>條款隱私權政策與安全性</li>
+              <li className='footer-font'>測試新功能</li>
+
+            </ul>
+
+
+          </div>
+
+        </div>
 
       </div>
     );
