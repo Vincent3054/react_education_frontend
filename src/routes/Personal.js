46b6d6f43c54e5344453f26974a066a648e810fd
yuchun <yuchun@youteki-MacBook-Air.local>
2020-04-25 19:06:09 +0800

    yuchun's avatar recordlist update 425 #04基本資料更新

--- src/routes/Personal.js
+++ src/routes/Personal.js
@@ -25,28 +25,35 @@ export default class Personal extends Component {
                             <label>
                                 <span className="font">姓名</span>
                             </label>
-                            <input className="input" type="text"  />
+                            <input className="input" type="text" />
                         </div>
                         <div className="boxtwo">
                             <span className="font">聯絡電話</span>
-                            <input className="input" type="text"  />
+                            <input className="input" type="text" />
                         </div>
                         <div className="boxthree">
                             <span className="font">電子郵件</span>
-                            <input className="input" type="text"  />
+                            <input className="input" type="text" />
                         </div>
                         <div className="boxfour">
                             <span className="font">班級</span>
-                            <input className="input" type="text"  />
+                            <input className="input" type="text" />
                         </div>
                         <div className="boxfive">
                             <span className="font">班級代號</span>
-                            <input className="input" type="text"  />
+                            <input className="input" type="text" />
                         </div>
                         <div className="boxsix">
                             <span className="font">性別</span>
-                            <input className="input" type="text"  />
+                            <input className="radio" name="radio" type="radio" />
+                            <label for="radio-2" className="radio-label">男</label>
+                            <input className="radio" name="radio" type="radio" />
+                            <label for="radio-2" className="radio-label">女</label>
                         </div>
+
+                    </div>
+                    <div className="btnsend">
+                    <button class="button button5">儲存變更</button>
                     </div>
                 </div>
             </Layout>
