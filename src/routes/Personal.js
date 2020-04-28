bcf5a27f35ebc2153e39882ec543c46630ac8afa
yuchun <yuchun@youteki-MacBook-Air.local>
2020-04-28 11:13:18 +0800

    recordlist update 427 #07 編輯基本資料

--- src/routes/Personal.js
+++ src/routes/Personal.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../layouts/Layout';
 import './Personal.css';
+import user from '../Assets/user.png';
 
 // import Personal from '../components/Studenlist';
 
 export default class Personal extends Component {
     static = {
         nane: "",
-
-
     }
     render() {
 
@@ -18,41 +17,46 @@ export default class Personal extends Component {
                 <div className="personal">
                     <div className="body">
                         <h1>個人資訊</h1>
-
                     </div>
                     <div className="main">
-                        <div className="boxone">
-                            <label>
+                        <div className="user">
+                            <img src={user} className="userimg" />
+                        </div>
+                        <div className="content">
+                            <div className="boxone">
                                 <span className="font">姓名</span>
-                            </label>
-                            <input className="input" type="text" readonly="readonly" value="歐俞均" />
-                        </div>
-                        <div className="boxtwo">
-                            <span className="font">聯絡電話</span>
-                            <input className="input" type="text" readonly="readonly" value="0975019879" />
-                        </div>
-                        <div className="boxthree">
-                            <span className="font">電子郵件</span>
-                            <input className="input" type="text" readonly="readonly" value="s1310831014@gms.nutc.edu.tw" />
-                        </div>
-                        <div className="boxfour">
-                            <span className="font">班級</span>
-                            <input className="input" type="text" readonly="readonly" value="資管三A" />
-                        </div>
-                        <div className="boxfive">
-                            <span className="font">班級代號</span>
-                            <input className="input" type="text" readonly="readonly" value="301" />
-                        </div>
-                        <div className="boxsix">
-                            <span className="font">性別</span>
-                            <input className="radio" name="radio" type="radio" />
-                            <label for="radio-2" className="radio-label">男</label>
-                            <input className="radio" name="radio" type="radio" />
-                            <label for="radio-2" className="radio-label">女</label>
+                                <span className="font">：</span>
+                                <span className="font">歐俞均</span>
+                            </div>
+                            <div className="boxtwo">
+                                <span className="font">聯絡電話</span>
+                                <span className="font">：</span>
+                                <span className="font">0975019879</span>
+                            </div>
+                            <div className="boxthree">
+                                <span className="font">電子郵件</span>
+                                <span className="font">：</span>
+                                <span className="font">ouyujyun@gmail.com</span>
+                            </div>
+                            <div className="boxfour">
+                                <span className="font">班級</span>
+                                <span className="font">：</span>
+                                <span className="font">資管三A</span>
+                            </div>
+                            <div className="boxfive">
+                                <span className="font">班級代號</span>
+                                <span className="font">：</span>
+                                <span className="font">101</span>
+                            </div>
+                            <div className="boxsix">
+                                <span className="font">性別</span>
+                                <span className="font">：</span>
+                                <span className="font">女</span>
+                            </div>
                         </div>
 
                     </div>
-                 
+
                 </div>
             </Layout>
         );
