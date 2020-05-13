3ca910e5786b81c270433b0d28ae2038369bd6a1
jetsql <perry1005@gmail.com>
2020-05-13 20:34:27 +0800

    Revert ""
    This reverts commit bf326d44366b359e8df66b9c037a944dcfc56c35.

--- WebApplication5/Controllers/AdminController.cs
+++ WebApplication5/Controllers/AdminController.cs
@@ -16,8 +16,6 @@ namespace WebApplication5.Controllers
              "*"                      // HTTP methods   允許使用方法
                                       //,"bar",                  // Response headers
                                       //SupportsCredentials = true  // Allow credentials
-
-
         )]
         //引用CorsHandle.cs
         [CorsHandle]
