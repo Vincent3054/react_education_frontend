e8e20148ed1c294988302720c5319b9a746450b2
yuchun <yuchun@youteki-MacBook-Air.local>
2020-06-15 19:35:28 +0800

    更新2019/06/15///

--- src/routes/ClassList/AddIndividualTeach.js
+++ src/routes/ClassList/AddIndividualTeach.js
@@ -2,100 +2,25 @@ import React, { Component } from "react";
 import Layout from "../../layouts/Layout";
 import "./AddIndividualTeach.css";
 
-// import Individual from '../components/Studenlist';
+// import AddIndividualTeach from '../components/Studenlist';
 
 export default class AddIndividualTeach extends Component {
   state = {
-    nane: "",
-      Category:"",//"輔導類別中輟",
-      Title:"",//"輔導標題",
-      Content:"",//"輔導內容",
-      Abstract:"",//"輔導摘要",
-      Category:"",//"諮詢類別"
-  };
-  render() {
-    axios.get(`http://studytutor_backend.hsc.nutc.edu.tw/api/Record?Reservation_Id=2`, {
-      headers: {
-        Authorization: JSON.parse(localStorage.getItem("Token")),
-      }
-    })
-      .then((res) => {
-        // console.log(res.data.Data.DataList);
-        const datalist = res.data.Data.DataList;
-        this.setState({
-          classlist: datalist
-        }, () => {
-          // console.log(this.state.classlist)
-        })
-      }).catch((err) => {
-        // console.error({ err }, 90);
-      })
-
   }
-
+  render() {
 
 
     return (
       <Layout>
         <div className="AddIndividualTeach">
+          <span><div className="titlename">個人輔導紀錄</div></span>
           <div className="body">
-            <div className="title">
-              <span className="titlename">學生列表</span>
-            </div>
-            <div className="tablecontent">
-            <form className="form" onSubmit={this.handleSubmit}>
-              <span className="title">學生姓名</span>
-              <div class="close" type="button" onClick={this.alterData}></div>
-              <div className="list">
-                <span className="list-text">輔導時間</span>
-                <input className="input" type="text" onChange={(e) => {
-                  this.setState({ Class_Id: e.target.value });
-                }}
-                  value={Class_Id}></input>
-              </div>
-              <div className="list">
-                <span className="list-text">填寫人</span>
-                <input className="input" type="text" onChange={(e) => {
-                  this.setState({ Grade: e.target.value });
-                }}
-                  value={Grade}></input>
-              </div>
-              <div className="list">
-                <span className="list-text">標題</span>
-                <input className="input" type="text" onChange={(e) => {
-                  this.setState({ ClassName: e.target.value });
-                }}
-                  value={ClassName}></input>
-              </div>
-              <div className="list">
-                <span className="list-text">類別</span>
-                <input className="input" type="text" onChange={(e) => {
-                  this.setState({ Teacher: e.target.value });
-                }}
-                  value={Teacher}></input>
-                <div className="list">
-                  <span className="list-text">晤談內容</span>
-                  <textarea id="message" name="message" placeholder="Your Message to Us" className="input" type="text" onChange={(e) => {
-                    this.setState({ Teacher: e.target.value });
-                  }}
-                    value={Teacher}>
-                  </textarea>
-                </div>
-                <div className="list">
-                  <span className="list-text">摘要內容</span>
-                  <textarea id="message" name="message" placeholder="Your Message to Us" className="input" type="text" onChange={(e) => {
-                    this.setState({ Teacher: e.target.value });
-                  }}
-                    value={Teacher}>
-                  </textarea>
-                </div>
-                <div className="list">
-                  <button className="login-btn" >
-                    送出
-                  </button>
-                </div>
-              </form>
-              </div>
+          <form action="get.php" method="get">
+          姓名：<input type='text'/><br/>
+          電話：<input type='text'/><br/>
+          <p><input type='submit' vÎalue='送出表單'/></p>
+          </form>
+            <div className="con"></div>
           </div>
         </div>
       </Layout>
